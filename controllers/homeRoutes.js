15b3dbc9cf1868aa106e28af9bf30e409778f304
mariamdawood <mariam.miladd@gmail.com>
2023-10-11 22:52:10 -0500

    Handlebars updates

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -69,14 +69,14 @@ router.get("/profile", withAuth, async (req, res) => {
   }
 });
 
-router.get("/login", (req, res) => {
+router.get("/signin", (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
     res.redirect("/profile");
     return;
   }
 
-  res.render("login");
+  res.render("signin");
 });
 
 // Path for region // cusines
