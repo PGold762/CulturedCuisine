69d83a99fd8a56268152d10579a0130aeb84c831
Lucas Jerome Lovelace <lucasjeromelovelace@Lucass-Air.attlocal.net>
2023-10-14 13:57:21 -0500

    updated routes

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,62 +1,26 @@
-// const { create } = require("domain");
-
-// const signupFormHandler = async (event) => {
-//   event.preventDefault();
-
-//   const response = await fetch('/api/users/signout', {
-//     method: 'POST',
-//     headers: { 'Content-Type': 'application/json' },
-//   });
-
-//   if (response.ok) {
-//     document.location.replace('/');
-//   } else {
-//     alert(response.statusText);
-//   }
-
-//   const firstName = document.querySelector('#first-name').value.trim();
-//   const lastName = document.querySelector('#last-name').value.trim();
-//   const email = document.querySelector('#sign-up-email').value.trim();
-//   const password = document.querySelector('#sign-up-password').value.trim();
-//   const createButton = document.querySelector('#create-button').value;
-
-//   if (firstName && lastName && email && password) {
-//     const response = await fetch('/api/users', {
-//       method: 'POST',
-//       body: JSON.stringify({ firstName, lastName, email, password }),
-//       headers: { 'Content-Type': 'application/json' },
-//     });
-
-//     if (response.ok) {
-//       document.location.replace('/profile');
-//     } else {
-//       alert(response.statusText);
-//     }
-//   }
-// };
-
-// const signout = async () => {
-
-// };
-
-
-// const createButton = document.querySelector('#create-button');
-// const signupForm = document.querySelector('.sign-up-form');
-
-// createButton.addEventListener('click', signupFormHandler);
-// signupForm.addEventListener('submit', signupFormHandler);
+const { create } = require("domain");
 
 const signupFormHandler = async (event) => {
   event.preventDefault();
 
-  // Collect values from the signup form
+  const response = await fetch('/api/users/signout', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+  });
+
+  if (response.ok) {
+    document.location.replace('/');
+  } else {
+    alert(response.statusText);
+  }
+
   const firstName = document.querySelector('#first-name').value.trim();
   const lastName = document.querySelector('#last-name').value.trim();
   const email = document.querySelector('#sign-up-email').value.trim();
   const password = document.querySelector('#sign-up-password').value.trim();
+  const createButton = document.querySelector('#create-button').value;
 
   if (firstName && lastName && email && password) {
-    // Send a POST request to the API endpoint for user creation
     const response = await fetch('/api/users', {
       method: 'POST',
       body: JSON.stringify({ firstName, lastName, email, password }),
@@ -64,7 +28,6 @@ const signupFormHandler = async (event) => {
     });
 
     if (response.ok) {
-      // If successful, redirect the browser to the profile page
       document.location.replace('/profile');
     } else {
       alert(response.statusText);
@@ -72,8 +35,45 @@ const signupFormHandler = async (event) => {
   }
 };
 
+const signout = async () => {
+
+};
+
+
 const createButton = document.querySelector('#create-button');
 const signupForm = document.querySelector('.sign-up-form');
 
 createButton.addEventListener('click', signupFormHandler);
 signupForm.addEventListener('submit', signupFormHandler);
+
+// const signupFormHandler = async (event) => {
+//   event.preventDefault();
+
+//   // Collect values from the signup form
+//   const firstName = document.querySelector('#first-name').value.trim();
+//   const lastName = document.querySelector('#last-name').value.trim();
+//   const email = document.querySelector('#sign-up-email').value.trim();
+//   const password = document.querySelector('#sign-up-password').value.trim();
+
+//   if (firstName && lastName && email && password) {
+//     // Send a POST request to the API endpoint for user creation
+//     const response = await fetch('/api/users', {
+//       method: 'POST',
+//       body: JSON.stringify({ firstName, lastName, email, password }),
+//       headers: { 'Content-Type': 'application/json' },
+//     });
+
+//     if (response.ok) {
+//       // If successful, redirect the browser to the profile page
+//       document.location.replace('/profile');
+//     } else {
+//       alert(response.statusText);
+//     }
+//   }
+// };
+
+// const createButton = document.querySelector('#create-button');
+// const signupForm = document.querySelector('.sign-up-form');
+
+// createButton.addEventListener('click', signupFormHandler);
+// signupForm.addEventListener('submit', signupFormHandler);
